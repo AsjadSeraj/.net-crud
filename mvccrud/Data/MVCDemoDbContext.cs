b9d90d036fc25887c37321365f2670a10845e7e2
AsjadSeraj <asjadseraj1652001@gmail.com>
2023-08-30 00:02:50 +0530

    first commit

---
+++ mvccrud/Data/MVCDemoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using mvccrud.Models.Domain;
+
+namespace mvccrud.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
